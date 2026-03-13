b8f6322f29dded9574459d69921b4fe1baeecf87
mraihandhai <67958239+mraihandhai@users.noreply.github.com>
2026-03-13 11:45:03 +0000

    1

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
-import joblib
-import sklearn
+import joblib, sklearn, groq, os
 
 model = joblib.load("foodexp.pkl")
 
@@ -31,6 +30,20 @@ def wrong():
 @app.route("/econ",methods=["GET","POST"])
 def econ():
     return(render_template("econ.html"))
+
+@app.route("/chatbot",methods=["GET","POST"])
+def chatbot():
+    return(render_template("chatbot.html"))
+
+os.environ["GROQ_API_KEY"] = ""
+
+@app.route("/roe",methods=["GET","POST"])
+def roe():
+    return(render_template("roe.html"))
+
+@app.route("/generalQuestion",methods=["GET","POST"])
+def generalQuestion():
+    return(render_template("generalQuestion.html"))
 
 @app.route("/foodExp",methods=["GET","POST"])
 def foodExp():
